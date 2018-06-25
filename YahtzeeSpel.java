2c9790f2c8d79c16792cd7e4ff5e77abed050ce8
felixvl <digitaleservice@gmail.com>
2018-06-25 12:41:22 +0200

    eerste random toegevoegd

--- YahtzeeSpel.java
+++ YahtzeeSpel.java
@@ -1,9 +1,11 @@
 package yahtzeeHuiswerk;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class YahtzeeSpel {
 	Scanner scanner = new Scanner(System.in);
+	Random random = new Random();
 	
 	void spelen() {
 		System.out.println("spelen");
@@ -14,8 +16,17 @@ public class YahtzeeSpel {
 			switch(input) {
 				case "q":
 					speelDoor = false;
+					break;
+				case "y":
+					System.out.println(werpen());
+					break;
+				default:
+					System.out.println("ongeldige invoer");
 			}
 		}
 		System.out.println("Bedankt voor het spelen");
+	}
+	int werpen() {
+		return random.nextInt(6)+1;
 	}
 }
